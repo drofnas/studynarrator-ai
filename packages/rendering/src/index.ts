375a904dcf6ed418b9a7b74c468b356284cbf59e
drofnas <drofnas@gmail.com>
2026-08-22 19:39:41 -0700

    fix(rendering): preserve render plan store barrel exports

--- packages/rendering/src/index.ts
+++ packages/rendering/src/index.ts
@@ -1,9 +1,3 @@
 export * from "./ffmpeg.js";
 export * from "./speechCache.js";
-export {
-  createPcmSilence,
-  createRenderPlanStore,
-  type RenderPlanStore,
-  withProjectSnapshotHash,
-  withRenderPlanHash,
-} from "./renderPlanStore.js";
+export * from "./renderPlanStore.js";
